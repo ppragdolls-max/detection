807bbb3358ed5f5d54980b2d508938da3955fe1a
ppragdolls-max <ppragdolls@gmail.com>
2025-10-23 16:57:41 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,77 +1,68 @@
 import time
 import requests
 import os
-import json
+import uuid
 from io import BytesIO
 from PIL import Image
 from ultralytics import YOLO
 import cv2 as cv
-import uuid
-# token = "1000.780f99c9e12d989c5cd51ec3a5304448.f70032f5653c35e215dd0c8b776101b4"
-# url1 = "https://accounts.zoho.in/oauth/v2/token?grant_type=authorization_code&client_id=1000.HXW46KJBW0PAQ5V0FXI422KCD2RF2A&client_secret=81e2a7b73552a4df1518d7140bb8a53141a79b260b&code="+token
-# myobj1 = {'somekey': 'somevalue'}
-# x = requests.post(url1, json = myobj1)
-# print(x.json())
-# if "refresh_token" in x.json():
-#     refresh_token = x.json()["refresh_token"]
-#     print("Refresh Token:", refresh_token)  
-while True:  # <-- loop continuously
+
+# Load YOLO model once (avoid reloading every image)
+model = YOLO("yolo11n.pt")
+
+while True:
     try:
+        # Refresh Zoho token
         refresh_token = "1000.254511476b2ff69e88275cf68a32eb75.3e41f070a3c87d4b026e5bafaa826a5d"
-        url = "https://accounts.zoho.in/oauth/v2/token?refresh_token="+refresh_token+"&client_id=1000.HXW46KJBW0PAQ5V0FXI422KCD2RF2A&client_secret=81e2a7b73552a4df1518d7140bb8a53141a79b260b&grant_type=refresh_token"
-        myobj = {'somekey': 'somevalue'}
-        x = requests.post(url, json = myobj)
-        if "access_token" in x.json():
-            access_token = x.json()["access_token"]
-        api_headers = {
-            "Authorization": "Zoho-oauthtoken "+access_token
-        }
-        report_response = requests.get("https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report", headers=api_headers)
+        url = f"https://accounts.zoho.in/oauth/v2/token?refresh_token={refresh_token}&client_id=1000.HXW46KJBW0PAQ5V0FXI422KCD2RF2A&client_secret=81e2a7b73552a4df1518d7140bb8a53141a79b260b&grant_type=refresh_token"
+        x = requests.post(url)
+        access_token = x.json().get("access_token")
+        api_headers = {"Authorization": "Zoho-oauthtoken " + access_token}
+
+        # Fetch records from Zoho
+        report_response = requests.get(
+            "https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report",
+            headers=api_headers
+        )
         var = report_response.json()
+
         if "data" in var:
             for record in var["data"]:
                 if "ID" in record:
                     id = record["ID"]
-                    im = record["upload_Image"]
-                    image_response = requests.get("https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report/"+id+"/upload_Image/download", headers=api_headers)
+                    image_response = requests.get(
+                        f"https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report/{id}/upload_Image/download",
+                        headers=api_headers
+                    )
                     img = Image.open(BytesIO(image_response.content))
-                    random_name = str(uuid.uuid4()) + ".jpg"
-                    # ############### Analysing ##################
-                    model = YOLO("yolo11n.pt")
-                    vid = img
-                    result = model(vid)
+                    img_cv = cv.cvtColor(np.array(img), cv.COLOR_RGB2BGR)
+
+                    # Run YOLO
+                    result = model(img_cv)
                     arr_frame = result[0].plot()
+
+                    # Save to /tmp
+                    random_name = f"/tmp/{uuid.uuid4()}.jpg"
                     cv.imwrite(random_name, arr_frame)
 
-                    cv.waitKey(0)
-                    cv.destroyAllWindows()
-                    ############### Uploading ##################
+                    # Upload processed image
                     up_refresh_token = "1000.0ec80a6e0bf93bfa7b8397d8d7d5f08c.5b3714b167919f0e2c24b74f808d90db"
-                    url2 = "https://accounts.zoho.in/oauth/v2/token?refresh_token="+up_refresh_token+"&client_id=1000.HXW46KJBW0PAQ5V0FXI422KCD2RF2A&client_secret=81e2a7b73552a4df1518d7140bb8a53141a79b260b&grant_type=refresh_token"
-                    myobj2 = {'somekey': 'somevalue'}
-                    xy = requests.post(url2, json = myobj2)
-                    if "access_token" in xy.json():
-                        up_access_token = xy.json()["access_token"]
-                    api_headers1 = {
-                        "Authorization": "Zoho-oauthtoken "+up_access_token
-                    }
-                    path = os.path.join("D:\\Training", random_name)
-                    print("Uploading Image from path:", path)
-                    parameters = {
-                        "skip_workflow": "[\"schedules\",\"form_workflow\"]"
-                    }
+                    url2 = f"https://accounts.zoho.in/oauth/v2/token?refresh_token={up_refresh_token}&client_id=1000.HXW46KJBW0PAQ5V0FXI422KCD2RF2A&client_secret=81e2a7b73552a4df1518d7140bb8a53141a79b260b&grant_type=refresh_token"
+                    xy = requests.post(url2)
+                    up_access_token = xy.json().get("access_token")
+                    api_headers1 = {"Authorization": "Zoho-oauthtoken " + up_access_token}
 
-                    file_path = path
-
-                    with open(file_path, 'rb') as file:
+                    with open(random_name, 'rb') as file:
                         files = {'file': file}
+                        params = {"skip_workflow": "[\"schedules\",\"form_workflow\"]"}
+                        requests.post(
+                            f"https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report/{id}/Detected_Image/upload",
+                            headers=api_headers1,
+                            files=files,
+                            params=params
+                        )
 
-                        try:
-                            response = requests.post("https://www.zohoapis.in/creator/v2.1/data/twinpixelsstudio/detection/report/Image_Upload_Report/"+id+"/Detected_Image/upload", headers=api_headers1, files=files, params=parameters)
-                        except:
-                            print("Exception while making the API request.")
-    except:
-        print("Exception while making the API request.")
+    except Exception as e:
+        print("Error:", e)
 
-    # Wait a few seconds before checking again
     time.sleep(10)
